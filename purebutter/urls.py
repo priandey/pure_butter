98246663c9f904caae6226f8c066a0b4e949c749
priandey <riandeypierre@gmail.com>
2019-07-03 09:29:38 +0000

    Added sentry config

--- purebutter/urls.py
+++ purebutter/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+def trigger_error(request):
+    division_by_zero = 1/0
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('favorites/', include('favorite.urls')),
+    path('sentry-debug/', trigger_error),
 ]
